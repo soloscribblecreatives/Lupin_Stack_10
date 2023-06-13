0bbde883646bb408c64894a198f57d6c7d5fecb8
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2023-06-14 02:18:50 +0530

    Lasminext

--- global.js
+++ global.js
@@ -1,3 +1,7 @@
+
+var arrSurveyQuestions = {
+   1: "Brand name for Lasmiditan migraine therapy?"
+};
 /*Code by android developers start here*/
 var startLoc = null;
 //var contentName = '152';
@@ -9,11 +13,11 @@ var currentContentNSlide ='';
 
 //custom slides changes begins here....
 
-//alert("++++++++++++"+custcomslideflag1+"+++++++custcomslideid+++++++"+custcomslideid1);
+//alert("+++++currentContentId+++++++"+currentContentId+"+++++++contentName+++++++"+contentName);
 	if (typeof(localStorage.getItem("currentcustomslideflag"))!='undefined' &&  localStorage.getItem("currentcustomslideflag") =='true'){
 		var custcomslideid1=parseInt(localStorage.getItem("currentcontentcustomslideId"));
-		
 		//step 2:
+
 		currentContentNSlide = currentContentId+"_"+contentName+"_"+custcomslideid1;
 		//step 2 ends here
 		localStorage.setItem("current",currentContentNSlide);
@@ -26,12 +30,7 @@ var currentContentNSlide ='';
 		localStorage.setItem("current",currentContentNSlide);
 		localStorage.setItem("currentslide",'1');
 	}
-	
-//custom slides changes ends here....
 
-/* currentContentNSlide = contentName+"_"+'1';
-localStorage.setItem("current",currentContentNSlide);
-localStorage.setItem("currentslide",'1'); */
 checkClickThrough();
 
 document.getElementById("main_content").addEventListener("touchmove", touchHandler, false);
@@ -99,7 +98,6 @@ function touchHandler(e) {
 		console.log("swipeleft"+localStorage.getItem("currentslide"));
 		localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
 		//step 4 ends here
-
 		//alert("swipeleft");
 		//myconsole("swipeleft");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -117,7 +115,6 @@ function touchHandler(e) {
 		console.log("swiperight"+localStorage.getItem("currentslide"));
 		localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
 		//step 5 ends here 
-
 			//alert("swiperight");
 		//myconsole("swiperight");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -138,61 +135,51 @@ function touchHandler(e) {
 	});
 
 
+
+
 });
+
+
 
 
 //step 6:-
 function toCaptureTime(page_id){
-	
 	var currentSlideNo = page_id;
-
 	var startTime = Date.now();
-
-
-	var temp = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
 	
+	//alert("===currentSlideNo===="+currentSlideNo);
+	//alert("===startTime===="+startTime);
+	var temp = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
 	if(temp == null){
-		
 		if (currentSlideNo!=0){
-
-
-
 			localStorage.setItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo ,startTime);
-
-			//to capture start time of slide in db format
 			var startTimeInDBFormat = currentTimeInDatabaseFormat();
-			//alert(startTimeInDBFormat);
-
-
-
-
 			localStorage.setItem(currentContentId+"_"+contentName+"_StartTime_"+currentSlideNo ,startTimeInDBFormat);
 		}
 }
-else
-{
-var existingTime = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
-var newTime = Date.now();
-var newSlideTime = (newTime - existingTime);
-var endTimeInDBFormat = currentTimeInDatabaseFormat();
-var EndTimeNext = localStorage.getItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
-console.log("++++++++EndTimeNext++++++++"+EndTimeNext+"++++++currentContentId+++"+currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
-if(EndTimeNext == null){
-localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+currentSlideNo ,(newSlideTime/1000) );
+else{
+	var existingTime = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
+	var newTime = Date.now();
+	var newSlideTime = (newTime - existingTime);
+	var endTimeInDBFormat = currentTimeInDatabaseFormat();
+    var EndTimeNext = localStorage.getItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
+		//alert("===newSlideTime===="+newSlideTime);
+	//alert("===EndTimeNext===="+EndTimeNext);
+    console.log("++++++++EndTimeNext++++++++"+EndTimeNext+"++++++currentContentId+++"+currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
+   
+   if(EndTimeNext == null){
+	localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+currentSlideNo ,(newSlideTime/1000) );
+	localStorage.setItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo ,endTimeInDBFormat);
+	}
 
-localStorage.setItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo ,endTimeInDBFormat);
-}
-
-if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.getItem('currentslide')!='' && localStorage.getItem('currentslide')>= currentSlideNo){
-
-
+    if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.getItem('currentslide')!='' && localStorage.getItem('currentslide')>= currentSlideNo){
 	var nextSlideNo = currentSlideNo;
 
-}else{
-
+    }else{
 	var nextSlideNo = currentSlideNo + 1 ;
 	
  } 
+ 
 	if(nextSlideNo <= 1){//number 3 is number of total slides present
 	// alert(nextSlideNo);
 	var tempNext = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo);
@@ -203,7 +190,6 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 				var nextSlideStartTime =  Date.now();
 				localStorage.setItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo ,nextSlideStartTime);
 				localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+nextSlideNo ,0);
-				//to capture start time of next slide in db format
 				var startTimeNextInDBFormat = currentTimeInDatabaseFormat();
 				localStorage.setItem(currentContentId+"_"+contentName+"_StartTime_"+nextSlideNo ,startTimeNextInDBFormat);
 			}
@@ -211,58 +197,55 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 	}
 }
 
-
 }
-//step 6 ends here ..
+//step ends..
 
 
 function go_nav(direction) {
-
-	
-//custom slide changes continues here....
 	
 	if (typeof(localStorage.getItem("currentcustomslideflag"))!='undefined' &&   localStorage.getItem("currentcustomslideflag") =='true'){
 
-				var custcomslideid=parseInt(localStorage.getItem("currentcontentcustomslideId"));
-			
-				var page_id =  custcomslideid;
+	var custcomslideid=parseInt(localStorage.getItem("currentcontentcustomslideId"));
+
+		var page_id =  custcomslideid;
 		}else{
 			
 				var page_id =  parseInt($("#wrapper").attr("rel"));
 		}	
 		
-//custom slide changes ends here....
-
-	//step 7:-
-	//toCaptureTime(page_id);
 	console.log("swipeleft"+localStorage.getItem("currentslide"));
 	localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
-	//step 7 ends here 
+	//step 7 ends here
 //localStorage.setItem(contentName+"_slideNo_"+currentSlideNo ,n);
 var flag=0;
+
+
 if(direction == 'b') {
 
-//custom slide changes continues here....
-
-		//alert("+++++bhitor reee +++++++"+custcomslideflag+"+++++++custcomslideid+++++++"+custcomslideid);
 	if (typeof(localStorage.getItem("currentcustomslideflag"))!='undefined' &&    localStorage.getItem("currentcustomslideflag") =='true'){
 		flag==0
 		localStorage.setItem("gotoNextPrevBrand" ,2);//if one than next if 2 than prev
 		window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
 
 
-	}else{
-	if(page_id == 1){
-	localStorage.setItem("gotoNextPrevBrand" ,2);//if one than next if 2 than prev
-	window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
+	}
+			else{
+			if(page_id >= 0){
+				page_id = page_id - 1;
+				if(page_id == 0){
+					flag=2;
+				}
+			}
 
+	 if(flag == 2){
+        localStorage.setItem("gotoNextPrevBrand" ,2);//if one than next if 2 than prev
+        //flag == 0;
+		window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
+    }else{
+        localStorage.setItem("gotoNextPrevBrand" ,0);
+    }
 
-}else{
-	localStorage.setItem("gotoNextPrevBrand" ,0);//if one than next if 2 than prev
-}
-}
-	
-//custom slide changes ends here....
+	}
 }else {
 	
 //custom slide changes continues here....
@@ -274,22 +257,27 @@ if(direction == 'b') {
 		window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
 	}
 	
-//custom slide changes ends here....
-else{
-	if(page_id == 1){
-	 localStorage.setItem("gotoNextPrevBrand" ,1);//if one than next if 2 than prev
-	 window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
-}else{
+		else{
+		if(page_id <= 1){
+		page_id = page_id + 1;
+		//alert(page_id);
+		if(page_id == 2){
+            flag=1;
+        }
+	}	
+	    if(flag == 1){
+        localStorage.setItem("gotoNextPrevBrand" ,1);//if one than next if 2 than prev
+         flag == 0;
+		 window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
+    }else{
+        localStorage.setItem("gotoNextPrevBrand" ,0);
+    }
 
-
-
-	localStorage.setItem("gotoNextPrevBrand" ,0);//if one than next if 2 than prev
 }
-	}
 }
+
 
 //step 8:
-
 currentContentNSlide = currentContentId+"_"+contentName+"_"+page_id;
 //step 8 ends here
 localStorage.setItem("current",currentContentNSlide);
@@ -311,29 +299,13 @@ var pg_content = set_pg_content(page_id);
 		$(".box3").click(function(event) {
 			open_page("",6)
 		});
-		$(".box4").click(function(event) {
-	 		open_page("",7)
-	 	});
-		$(".box5").click(function(event) {
-	 		open_page("",8)
-	 	});
-		$(".box6").click(function(event) {
-	 		open_page("",9)
-	 	});
-		$(".box7").click(function(event) {
-	 		open_page("",10)
-	 	});
-		$(".box8").click(function(event) {
-	 		open_page("",11)
-	 	});
 		
 	}
 	 checkClickThrough();
 }
 
 function set_pg_content(pg_id){
-//alert("++++++++++set_pg_content++++++++++"+pg_id);
-	//step 9:
+
 	if (typeof(localStorage.getItem("previousslide"))!='undefined'){
 		//to checked previous slide has god end time...
 		var previousslideid=localStorage.getItem("previousslide");
@@ -341,15 +313,17 @@ function set_pg_content(pg_id){
 		
 	}
 	toCaptureTime(pg_id);
+
+
 	//step 9 ends here..
+//alert("++++++++++set_pg_content++++++++++"+pg_id);
 $(".reference").removeClass("active");
 currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"></div><div class="video1"><video preload="auto" autoplay onplay="myFunction()" id="startVideo" width="1024" height="670"><source src="slide1/Part1.mp4" type="video/mp4"></video></div><div class="button"></div><div class="video2"><video preload="auto" id="endVideo" width="1024" height="670"><source src="slide1/Part2.mp4" type="video/mp4"></video></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png" width="1024" height="768" alt=""></div><div class="s1_2"><img src="slide1/s1_2.png" width="1024" height="768" alt=""></div><div class="s1_3"><img src="slide1/s1_3.png"></div><div class="s1_4"><img src="slide1/s1_4.png"></div><div class="s1_5"><img src="slide1/s1_5.png"></div><div class="s1_6"><img src="slide1/s1_7.png"></div><div class="s1_7"><img src="slide1/s1_7.png"></div><div class="s1_8"><img src="slide1/s1_7.png"></div><div class="s1_9"><img src="slide1/s1_6.png"></div><audio id="correct"><source src="slide1/Correct.mp3" type="audio/mpeg"></audio><audio id="wrong"><source src="slide1/Wrong.mp3" type="audio/mpeg"></audio><audio id="welcome"><source src="slide1/Welcome.mp3" type="audio/mpeg"></audio>';
 	break;
-
 }
 
 return content;
@@ -363,15 +337,17 @@ function showDiv2() {
    document.getElementById('welcomeDiv2').style.display = "block";
 }
 
+
 function open_page(url,page_id){
-	 // alert(page_id);
-	//step 10:
+	
+	count3=2;
+    count4=0;
 	if (typeof(localStorage.getItem("currentslide"))!='undefined'){
 		//to checked previous slide has god end time...
 		var slideid=localStorage.getItem("currentslide");
 		toCaptureTime(slideid);	
 	}
-	
+
 	// toCaptureTime(page_id);
 	 localStorage.setItem("currentslide",page_id);
 	 currentContentNSlide = currentContentId+"_"+contentName+"_"+page_id;
@@ -391,46 +367,80 @@ function open_page(url,page_id){
 		$(".box3").click(function(event) {
 			open_page("",6)
 		});
-		$(".box4").click(function(event) {
-	 		open_page("",7)
-	 	});
-		$(".box5").click(function(event) {
-	 		open_page("",8)
-	 	});
-		$(".box6").click(function(event) {
-	 		open_page("",9)
-	 	});
-		$(".box7").click(function(event) {
-	 		open_page("",10)
-	 	});
-		$(".box8").click(function(event) {
-	 		open_page("",11)
-	 	});
 	 }
 	  checkClickThrough();
 	}
 
+	var count3=2,count4=0;
+
+function open_page2(url,page_id,count){
+    count1=0;
+    count3=page_id+count-2;
+    count4=page_id+1;
+	 // alert(page_id);
+	if (typeof(localStorage.getItem("currentslide"))!='undefined'){
+		var slideid=localStorage.getItem("currentslide");
+		toCaptureTime(slideid);
+	}
+    count2=page_id;
+    count1=page_id+count-1;
+
+	localStorage.setItem("currentslide",page_id);
+	currentContentNSlide = currentContentId+"_"+contentName+"_"+page_id;
+	localStorage.setItem("current",currentContentNSlide);
+
+	$("#wrapper").attr("rel",page_id);
+	var content="";
+	var pg_content = set_pg_content(page_id);
+	$("#main_content").html(pg_content);
+	checkClickThrough();
+}
+	
+	
 	function checkClickThrough(){
 	var currentslide=localStorage.getItem("currentslide");
 	//alert(currentslide);
 	document.getElementById("click_through").innerHTML='';
 
-	if(currentslide == 1){
-	document.getElementById("click_through").innerHTML='';
-		}
-    if(currentslide == 2){
-	document.getElementById("click_through").innerHTML='';
-		}
-	}
+		if(currentslide == 1){
+		document.getElementById("click_through").innerHTML='<div class="slide02_inline_wraper" id="buttons">\
+		<div id="slide01_question01_choices01" class="control-group">\
+			<label class="control control_radio"><div class="lbl_pos"></div><input type="radio" id="slide01_radio01_01" name="checkB01" value="Lasmitame"/><div class="control_indicator"id="radio01" onclick="option1()"></div></label>\
+			<label class="control control_radio"><div class="lbl_pos"></div><input type="radio" id="slide01_radio01_02" name="checkB01" value="Lasmivow"/><div class="control_indicator"id="radio02" onclick="option2()"></div></label>\
+			<label class="control control_radio"><div class="lbl_pos"></div><input type="radio" id="slide01_radio01_03" name="checkB01" value="Lasmithink"/><div class="control_indicator"id="radio03" onclick="option3()"></div></label>\
+			<label class="control control_radio"><div class="lbl_pos"></div><input type="radio" id="slide01_radio01_04" name="checkB01" value="Lasminext"/><div class="control_indicator"id="radio04" onclick="option4()"></div></label>\
+		</div>\
+			<div class="submit_button" onclick="savedata(1,1,1);endTime1(1);hidesubmitonclick();"></div>\
+		</div>';
+		
+		$('#slide01_question01_choices01').delay(10).fadeIn();
 
-	function checkBtns(refNum){
-		switch(refNum){
-			case 1:
-			open_page('',1);
-            break;
-		}
-	}
+		$(document).on("click touchstart", "#slide01_question01_choices01 input[name]", function(){
+			if ($("input[name='checkB01']:checked").val()){
+				var test = $(this).val();	
+			}			
+		});
 
+		$(document).on("click", ".submit_button", function(){
+			$('.slide01_submit_popup_content').fadeIn();
+		});
+
+		$(document).on("click", ".slide01_submit_popup_close_btn_mask", function(){
+			$('.slide01_submit_popup_content').fadeOut();
+		});
+	}
+	
+}
+
+
+function checkBtns(refNum){
+	switch(refNum){
+		case 1:
+		open_page('',2);
+		break;
+	}
+}
+	
 	function currentSlide(){
 		var curr_id =  parseInt($("#wrapper").attr("rel"));
 		$(".slides").removeClass("active");
@@ -477,8 +487,6 @@ function currentTimeInDatabaseFormat(){//to get current time in dd-mm-yyyy hh:mm
 	return duration;
 }
 
-// new js
-
 $(document).ready(function(){
 	$('body').on('click','.touchbtn',function(){
 		$('.right_arrow').trigger( "click" );
@@ -490,31 +498,87 @@ $(document).ready(function(){
 	})
 })
 
-/*--------------------------Javascript Animation-----------------------------*/
 
-function myFunction(){
-	setTimeout(function () {
-		$('.button').css("display","block");
-	}, 15000);
-};
+function savedata(answer,type,questionNumber){
+	
+	
+ 	if(questionNumber == 1){
+		var selectedAnswer1 = document.querySelector('input[name = "checkB01"]:checked').value;
+		var varanswer = selectedAnswer1;
+	}
+	var question = arrSurveyQuestions[questionNumber];
+	localStorage.setItem("surveyQuestion_"+currentContentId+"_"+contentName+"_"+questionNumber,question);
+	localStorage.setItem("surveyAnswer_"+currentContentId+"_"+contentName+"_"+questionNumber,varanswer);
+	//toCaptureTime(2);
+    //document.getElementById('edit-count-checked-checkboxes').value=count;
 
-$(document).ready(function(){
-	$(document).on('click','.button',function(){
-		$(this).hide();
-		$(".video2").show();
-		var video1 = $("#startVideo").get(0);
-    	video1.pause();
-		var video2 = $("#endVideo").get(0);
-    	video2.play();
-	})
-})	
+}
 
-/* 
-$(document).ready(function() {
-	var vid = document.getElementById("startVideo");
-    vid.addEventListener("timeupdate", function() {
-		if(vid.currentTime=5) {
-			$(".button").show();
-		}
-	});
-}); */
+
+function closewindowslide(currentslide)
+{
+	toCaptureTime(currentslide);
+}
+function endTime1(currentSlideNo){
+		var existingTime = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
+		var newTime = Date.now();
+		var newSlideTime = (newTime - existingTime);
+		localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+currentSlideNo ,(newSlideTime/1000) );
+		var endTimeInDBFormat = currentTimeInDatabaseFormat();
+		localStorage.setItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo ,endTimeInDBFormat);
+
+}
+
+
+/*--------------------- animation javascript -----------------------*/
+
+function hidesubmitonclick()
+{
+	document.getElementById("welcome").play();
+	$('.submit_button').css("display","none");
+	$('.s1_3').css("display","block");
+	$('.s1_4').css("display","block");
+	$('.s1_5').css("display","block");
+}
+
+function option1() {
+	document.getElementById("wrong").play();
+	$('.s1_6').css("display","block");
+	$('.s1_7').css("display","none");
+	$('.s1_8').css("display","none");
+	$('.s1_9').css("display","none");
+	$('.submit_button').css("display","none");
+}
+
+function option2() {
+	document.getElementById("wrong").play();
+	$('.s1_6').css("display","none");
+	$('.s1_7').css("display","block");
+	$('.s1_8').css("display","none");
+	$('.s1_9').css("display","none");
+	$('.submit_button').css("display","none");
+}
+
+function option3() {
+	document.getElementById("wrong").play();
+	$('.s1_6').css("display","none");
+	$('.s1_7').css("display","none");
+	$('.s1_8').css("display","block");
+	$('.s1_9').css("display","none");
+	$('.submit_button').css("display","none");
+}
+
+function option4() {
+	document.getElementById("correct").play();
+	$('.s1_6').css("display","none");
+	$('.s1_7').css("display","none");
+	$('.s1_8').css("display","none");
+	$('.s1_9').css("display","block");
+	
+	$('#radio01').css("display","none");
+	$('#radio02').css("display","none");
+	$('#radio03').css("display","none");
+	$('#radio04').css("display","none");
+	
+	$(".submit_button,.slide01_next").fadeIn();
+}
