5c61cdd98d63634c606e6eeb50aa129fbb5eeeaf
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2024-04-23 17:08:07 +0530

    pyk

--- global.js
+++ global.js
@@ -1,5 +1,4 @@
 /*Code by android developers start here*/
-
 var startLoc = null;
 //var contentName = '152';
 //step 1:-
@@ -69,7 +68,7 @@ function touchHandler(e) {
 
 		$("#main_content").swipe({
 	   swipeLeft:function(event, direction, distance, duration, fingerCount) {
-		
+	
 		//alert("swipeleft");
 		//myconsole("swipeleft");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -83,7 +82,7 @@ function touchHandler(e) {
 	  },
 
 	  swipeRight:function(event, direction, distance, duration, fingerCount) {
-		
+		 
 			//alert("swiperight");
 		//myconsole("swiperight");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -104,14 +103,53 @@ function touchHandler(e) {
 	});
 });
 
+
+
 function go_nav(direction) {
 var page_id =  parseInt($("#wrapper").attr("rel"));
-			
+															 
+					
+				   
+ 
+										 
+ 
+																																	   
+
+   
+																					 
+   
+	
+								  
 		
+			
+													   
+	
+		
+									
+
+		   
+						  
+															   
+																			
+				   
+																  
 var flag=0;
 if(direction == 'b') {
 
+										 
 
+																									 
+																																		
+		 
+																				
+																																					
+
+
+  
+ 
+									
+
+	  
 	if(page_id >= 0){
 		page_id = page_id - 1;
 		//alert(page_id);
@@ -157,11 +195,22 @@ if(direction == 'b') {
 	
 }else {
 	
+										 
 
-	if(page_id <= 6){
+																																	 
+		 
+																				
+
+																																					
+  
+ 
+									
+
+	  
+	if(page_id <= 4){
 		page_id = page_id + 1;
 		//alert(page_id);
-		if(page_id == 7){
+		if(page_id == 5){
             flag=1;
         }
 	}
@@ -203,7 +252,10 @@ if(direction == 'b') {
 
 
 }
+ 
 
+
+		 
 
 
 $("#wrapper").attr("rel",page_id);
@@ -216,7 +268,7 @@ var pg_content = set_pg_content(page_id);
 }
 	//console.log("pg : "+page_id);
 	if(page_id==4){
-		/* $(".box2").click(function(event) {
+		$(".box2").click(function(event) {
 			open_page("",5)
 		});
 		$(".box3").click(function(event) {
@@ -236,7 +288,7 @@ var pg_content = set_pg_content(page_id);
 	 	});
 		$(".box8").click(function(event) {
 	 		open_page("",11)
-	 	}); */
+	 	});
 		
 	}
 	 checkClickThrough();
@@ -251,22 +303,16 @@ currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide1/s1_2.png"/></div><div class="wrap_3"><div class="s1_3"><img src="slide1/s1_3.png"/></div></div><div class="s1_4"><img src="slide1/s1_4.png"/></div><div class="wrap_5"><div class="s1_5"><img src="slide1/s1_5.png"/></div></div><div class="s1_6"><img src="slide1/s1_6.png"/></div><div class="wrap_7"><div class="s1_7"><img src="slide1/s1_7.png"/></div></div><div class="s1_8"><img src="slide1/s1_8.png"/></div><div class="wrap_9"><div class="s1_9"><img src="slide1/s1_9.png"/></div></div><div class="wrap_10"><div class="s1_10"><img src="slide1/s1_10.png"/></div></div><div class="s1_11"><img src="slide1/s1_11.png"/></div><div class="s1_12"><img src="slide1/s1_12.png"/></div><div class="s1_13"><img src="slide1/s1_13.gif"/></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide1/s1_2.png"/></div><div class="s1_3"><img src="slide1/s1_3.png"/></div>';
 	break;
 	case 2:
-	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="s1_1"><img src="slide2/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide2/s1_2.gif"/></div><div class="s1_3"><img src="slide2/s1_3.png"/></div><div class="s1_4"><img src="slide2/s1_4.png"/></div><div class="s1_5"><img src="slide2/s1_5.png"/></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="s1_1"><img src="slide2/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide2/s1_2.png"/></div><div class="s1_3"><img src="slide2/s1_3.png"/></div>';
 	break;
-	case 3:
-	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="s1_1"><img src="slide3/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide3/s1_2.gif"/></div><div class="s1_3"><img src="slide3/s1_3.png"/></div><div class="s1_4"><img src="slide3/s1_4.png"/></div><div class="s1_5"><img src="slide3/s1_5.png"/></div>';
+    case 3:
+	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="s1_1"><img src="slide3/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide3/s1_2.png"/></div><div class="s1_3"><img src="slide3/s1_3.png"/></div><div class="s1_4"><img src="slide3/s1_4.png"/></div><div class="s1_5"><img src="slide3/s1_5.png"/></div><div class="s1_6"><img src="slide3/s1_6.png"/></div><div class="s1_7"><img src="slide3/s1_7.png"/></div>';
 	break;
-	case 4:
-	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="s1_1"><img src="slide4/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide4/s1_2.png"/></div><div class="s1_3"><img src="slide4/s1_3.png"/></div>';
-	break;
-	case 5:
-	content='<link rel="stylesheet" type="text/css" href="slide5/slide5.css" media="screen"/><div class="s1_1"><img src="slide5/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide5/s1_2.gif"/></div><div class="s1_3"><img src="slide5/s1_3.png"/></div>';
-	break;
-	case 6:
-	content='<link rel="stylesheet" type="text/css" href="slide6/slide6.css" media="screen"/><div class="s1_1"><img src="slide6/s1_1.png" width="1024" height="768" alt=""/></div>';
+    case 4:
+	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="s1_1"><img src="slide4/s1_1.png" width="1024" height="768" alt=""/></div><div class="s1_2"><img src="slide4/s1_2.png"/></div><div class="s1_3"><img src="slide4/s1_3.png"/></div><div class="s1_4"><img src="slide4/s1_4.png"/></div><div class="s1_5"><img src="slide4/s1_5.png"/></div>';
 	break;
 }
 
@@ -309,6 +355,9 @@ function open_page(url,page_id){
 
 	//window.messageHandler.postMessage(JSON.stringify(params)); //pageswipe
 	
+						   
+							
+
 	 $("#wrapper").attr("rel",page_id);
 	 var content="";
 	 var pg_content = set_pg_content(page_id);
@@ -420,16 +469,32 @@ $(document).ready(function(){
 	})
 })
 
-//----------------------------------//
+/*--------------------- animation javascript -----------------------*/
 
-function pop_open() {
-	$('.popup').css('display','block');
-	$('.pop_open').css('display','none');
-	$('.pop_close').css('display','block');
+function s1_pop1() {
+	$('.s1_2').css("display","block");
+	$('.s1_c1ose1').css("display","block");
 }
 
-function pop_close() {
-	$('.popup').css('display','none');
-	$('.pop_open').css('display','block');
-	$('.pop_close').css('display','none');
+function s1_pop2() {
+	$('.s1_3').css("display","block");
+	$('.s1_c1ose1').css("display","block");
+}
+
+function s1_pop3() {
+	$('.s1_4').css("display","block");
+	$('.s1_c1ose1').css("display","block");
+}
+
+function s1_pop4() {
+	$('.s1_5').css("display","block");
+	$('.s1_c1ose1').css("display","block");
+}
+
+function s1_close1() {
+	$('.s1_2').css("display","none");
+	$('.s1_3').css("display","none");
+	$('.s1_4').css("display","none");
+	$('.s1_5').css("display","none");
+	$('.s1_c1ose1').css("display","none");
 }
